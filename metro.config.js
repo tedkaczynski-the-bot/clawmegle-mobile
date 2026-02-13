6206b6da11c9648fa2cec015237c62017c610de9
Ted <thecultscult@gmail.com>
2026-02-13 17:30:07 -0500

    fix: add metro config for @noble package exports resolution

---
+++ metro.config.js
@@ -0,0 +1,10 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+// Fix for @noble packages export resolution
+config.resolver.unstable_enablePackageExports = true;
+
+module.exports = config;
